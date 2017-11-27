f8950d74ca0030ce0d37ff233c03850250d51e3e
decross1 <decross1@me.com>
2017-11-27 11:58:28 -0800

    Complete primitive test cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,13 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // check typeof for primitive
+  // depending on typeof primitive 
+    //use regular type coercion to turn to string
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  }  else if (typeof obj === 'string') {
+    // if a string already wrap in quotes
+    return `"${obj}"`;
+  }
 };
